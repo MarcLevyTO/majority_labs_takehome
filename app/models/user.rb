b767af2b5b504f4bb3f491609280b1c6fc72882a
Marc Levy <marc.levy.to@me.com>
2020-07-08 04:48:22 -0400

    added password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,21 @@ class User < ApplicationRecord
   validates :username, uniqueness: { case_sensitive: false }
   validates :password, presence: true
 
+  validate :password_requirements_are_met
+
+  def password_requirements_are_met
+    rules = {
+      " must contain at least one lowercase letter"  => /[a-z]+/,
+      " must contain at least one uppercase letter"  => /[A-Z]+/,
+      " must contain at least one digit"             => /\d+/,
+      " must contain at least one special character" => /[^A-Za-z0-9]+/
+    }
+  
+    rules.each do |message, regex|
+      errors.add( :password, message ) unless password.match( regex )
+    end
+  end
+
   # Hide password from results
   def as_json(options = {})
     super(options.merge({ except: [:password_digest] }))
